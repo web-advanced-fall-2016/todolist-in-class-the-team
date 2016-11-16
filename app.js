c156f1a56b921d86a7125b88cfd45397004e6084
Saman Tehrani <samanrtehrani@gmail.com>
2016-11-16 18:39:00 -0500

    base scaffold added

---
+++ app.js
@@ -0,0 +1,21 @@
+const http = require('http');
+const express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+const port = 3000;
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+/*====================
+    YOUR CODE
+====================*/
+
+const server = http.createServer(app);
+server.listen(port, () => {
+    console.log(`Server running and listening at http://localhost:${port}/`);
+});
